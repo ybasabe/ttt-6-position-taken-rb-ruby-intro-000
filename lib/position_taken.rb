2d849f9576c2d6148c19eb3b76f937af8ba2dd01
dazzling-directory-8519 <yamirabasabe@gmail.com>
2020-01-28 16:46:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,3 +6,8 @@ def position_taken? (board,index)
 end
 
 
+puts position_taken?(ttt_board, 0)
+puts position_taken?(ttt_board, 1)
+puts position_taken?(ttt_board, 2)
+puts position_taken?(ttt_board, 3)
+puts position_taken?(ttt_board, 4)
